617900afa1b5398151544ab0cfcdc00ec65d0147
Bonsa Adugna Mosisa <bonsa.adugnam@gmail.com>
2024-04-30 10:56:47 +0300

    frist commit

---
+++ MethodP.java
@@ -0,0 +1,18 @@
+import java.lang.Math;
+import java.util.Scanner;
+
+public class MathodP {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to squaareroot calculator")
+        double number = 25.0;
+        double squareRoot = Math.sqrt(number);
+        System.out.println(Math.max(12,13));
+        System.out.println("Square root of the Number " + number + " is " + squareRoot);
+        int Num;
+        Scanner Keyb=new Scanner(System.in);
+        System.out.print("PLEASE ENTER YOUR NUMBER TO FIND SQORT :");
+        Num=Keyb.nextInt();
+        System.out.println("The Square root of "+ Num +" is "+ Math.sqrt(Num));
+    }
+}
